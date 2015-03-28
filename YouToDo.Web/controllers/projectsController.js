f31125e84176bae506be55bc6a2374ec32aad889
Alexander Teseyko <alexteko@gmail.com>
2015-03-28 16:09:02 +0300

    Projects from DB

--- YouToDo.Web/controllers/projectsController.js
+++ YouToDo.Web/controllers/projectsController.js
@@ -1,4 +1,10 @@
 ﻿'use strict';
 youToDoApp.controller('projectsController', ['$scope', 'projectsService', function ($scope, projectsService) {
-    $scope.projects = projectsService.getProjects();
+    $scope.projects = [];
+
+    projectsService.getProjects().then(function (results) {
+        $scope.projects = results.data;
+    }, function (error) {
+        //alert(error.data.message);
+    });
 }]);
--- YouToDo.Web/services/projectsService.js
+++ YouToDo.Web/services/projectsService.js
@@ -6,6 +6,20 @@ youToDoApp.factory('projectsService', ['$http', 'ngAuthSettings', function ($htt
     var projectsServiceFactory = {};
 
     var _getProjects = function () {
+
+    //    var array = [
+    //{
+    //    "Id": 1,
+    //    "Name": "Test1",
+    //    "Description": "Test1",
+    //    "ProjectLeader": 0
+    //},
+    //{
+    //    "Id": 3,
+    //    "Name": "Test3",
+    //    "Description": "Test3",
+    //    "ProjectLeader": 0
+    //}];
         return $http.get(serviceBase + 'api/projects').then(function (results) {
             return results;
         });       
