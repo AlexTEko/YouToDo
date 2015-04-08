9b31f3bad60055dd0e0af8cd6bb391f8cc998134
Alexander Teseyko <alexteko@gmail.com>
2015-04-08 12:55:02 +0300

    Add Project page to WebClient

---
+++ YouToDo/Scripts/controllers/projectController.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+youToDoApp.controller('projectController', ['$scope', 'projectService', '$stateParams', function ($scope, projectService, $stateParams) {
+    $scope.project = {};
+    $scope.showModalEdit = false;
+    $scope.formData = {};
+
+    var viewProject = function ($stateParams) {
+        var id = $stateParams.id;
+        projectService.getProject(id).then(function (results) {
+            $scope.project = results.data;
+        }, function (error) {
+            //alert(error.data.message);
+        });
+    };
+
+    var saveProject = function () {
+        projectService.saveProject($scope.formData, $scope.project.Id).success(function () {
+            $scope.showModalEdit = !$scope.showModalEdit;
+            viewProject($stateParams);
+        }, function (error) {
+            alert(error.data.message);
+        });
+    };
+
+    viewProject($stateParams);
+
+    $scope.editProject = function () {
+        $scope.formData = { Name: $scope.project.Name, Description: $scope.project.Description };
+        $scope.showModalEdit = !$scope.showModalEdit;
+    };
+
+    $scope.saveProject = function () {
+        saveProject(projectService);
+    };
+}]);
---
+++ YouToDo/Scripts/services/projectService.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+youToDoApp.factory('projectService', ['$http', 'ngAuthSettings', 'authService', function ($http, ngAuthSettings, authService) {
+
+    var projectServiceFactory = {};
+
+    var _getProject = function (id) {
+
+        return $http.get('api/projects/'+id).then(function (results) {
+            return results;
+        });
+    };
+
+    var _saveProject = function (formData, id) {
+        formData.Id = id;
+        formData.ProjectLeader = authService.authentication.userName;
+        return $http({
+            method: 'PUT',
+            url: 'api/projects/'+id,
+            data: formData,
+            headers: { 'Content-Type': 'application/json' }
+        })
+    };
+
+    projectServiceFactory.getProject = _getProject;
+    projectServiceFactory.saveProject = _saveProject;
+
+    return projectServiceFactory;
+
+}]);
